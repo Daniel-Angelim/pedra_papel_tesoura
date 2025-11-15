671f23bf6d5d5f9287a36f80c893a05a0686e720
Daniel Angelim <daniel.m.angelim1204@gmail.com>
2025-11-15 00:36:38 -0300

    Adicionada página de regras e otimização de código

--- assets/js/pedra_papel_tesoura.js
+++ assets/js/pedra_papel_tesoura.js
@@ -1,2 +1,25 @@
-alert("Hello world")
-alert("fazendo mudança no código")
+const modal = document.getElementById('modal')
+const mHeader = document.getElementById('mHeader')
+const mbody = document.getElementById('mBody');
+const fecharBtn = document.getElementById('fecharBtn');
+const mShadow = document.getElementById('mShadow');
+
+function ativarModal(){
+    modal.style.animation = "modalIn 0.5s";
+    mShadow.style.animation = "msIn 0.5s";
+
+    modal.style.display = "flex";
+    mShadow.style.display = "block";
+}
+
+function fecharModal(){
+    modal.style.animation = "modalOut 0.5s";
+    mShadow.style.animation = "msOut 0.5s";
+
+    setTimeout(() => {
+        modal.style.display = "none";
+    mShadow.style.display = "none";
+    },499)
+
+    
+}
