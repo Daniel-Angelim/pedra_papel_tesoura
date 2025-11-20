e463ba6193cdc1b31e3099ca657f24a7b68c365c
Daniel Angelim <daniel.m.angelim1204@gmail.com>
2025-11-20 18:19:53 -0300

    Alerta de espere o contador e modal de resultado configurados

--- assets/js/pedra_papel_tesoura.js
+++ assets/js/pedra_papel_tesoura.js
@@ -12,6 +12,31 @@ const tesoura1 = document.getElementById('Botao-Tesoura-1');
 const pedra2 = document.getElementById('Botao-Pedra-2');
 const papel2 = document.getElementById('Botao-Papel-2');
 const tesoura2 = document.getElementById('Botao-Tesoura-2');
+// Pontuação
+const placar1 = document.getElementById('jog-1');
+const placar2 = document.getElementById('jog-2');
+let ponto1 = 0;
+let ponto2 = 0;
+// resultados
+const result1 = document.getElementById('jogada1');
+const result2 = document.getElementById('jogada2');
+const resultFinal = document.getElementById('resultado');
+const placar = document.getElementById('placar');
+// Função pra zerar resultados
+function zerar(){
+    cont = 1;
+    controle = 0;
+    document.getElementById('txt-contagem').textContent = "";
+    setTimeout(() => {
+        result1.textContent = "Jogador 1 jogou: ";
+        result2.textContent = "Jogador 2 jogou: ";
+        resultFinal.textContent = "Resultado: ";
+    }, 4000);
+    setTimeout(() => {
+        placar.style.display = "none";
+    }, 3000);
+
+}
 // Animações painel de regras
 function ativarModal(){
     modal.style.animation = "modalIn 0.5s";
@@ -40,45 +65,58 @@ let cont = 1
 // funcões de alteração de valor
 // jogador 1
 function pedra_1(){
-    jogada_1 = 'pedra';
+    jogada_1 = 'Pedra';
     if (cont === 2){
-        verificar_resultado()
+        verificar_resultado();
+        setTimeout(() => {
+            zerar()}, 1000);
     }
     cont += 1;
+    
 }
 function papel_1(){
-    jogada_1 = 'papel';
+    jogada_1 = 'Papel';
     if (cont === 2){
-        verificar_resultado()
+        verificar_resultado();
+        setTimeout(() => {
+            zerar()}, 1000);
     }
     cont += 1;
 }
 function tesoura_1(){
-    jogada_1 = 'tesoura';
+    jogada_1 = 'Tesoura';
     if (cont === 2){
-        verificar_resultado()
+        verificar_resultado();
+        setTimeout(() => {
+            zerar()}, 1000);
     }
     cont += 1;
 }
 // jogador 2
 function pedra_2(){
-    jogada_2 = 'pedra';
+    jogada_2 = 'Pedra';
     if (cont === 2){
-        verificar_resultado()
+        verificar_resultado();
+        setTimeout(() => {
+            zerar()}, 1000);
     }
     cont += 1;
 }
 function papel_2(){
-    jogada_2 = 'papel';
+    jogada_2 = 'Papel';
     if (cont === 2){
-        verificar_resultado()
+        verificar_resultado();
+        setTimeout(() => {
+            zerar()}, 1000);
     }
     cont += 1;
 }
 function tesoura_2(){
-    jogada_2 = 'tesoura';
+    jogada_2 = 'Tesoura';
     if (cont === 2){
-        verificar_resultado()
+        verificar_resultado();
+        setTimeout(() => {
+            zerar()}, 1000);
     }
     cont += 1;
 }
@@ -105,23 +143,26 @@ tesoura2.addEventListener('click', function(){
 })
 // Verficando resultado
 function verificar_resultado(){
+    result1.textContent = result1.textContent + jogada_1;
+    result2.textContent = result2.textContent + jogada_2;
     if (controle === 1){    
         if(jogada_1 === jogada_2 && jogada_1 != null){
-            alert("empate")
+        resultFinal.textContent = resultFinal.textContent + "Empate";
         }
-        else if((jogada_1 === "pedra" && jogada_2 ==="tesoura") || (jogada_1==="papel" && jogada_2==="pedra") ||(jogada_1==="tesoura" && jogada_2==="papel")){
-            alert("Jogador 1 venceu")
+        else if((jogada_1 === "Pedra" && jogada_2 ==="Tesoura") || (jogada_1==="Papel" && jogada_2==="Pedra") ||(jogada_1==="Tesoura" && jogada_2==="Papel")){
+        resultFinal.textContent = resultFinal.textContent + "Jogador 1 venceu!";
+            ponto1 += 1;
         }
         else if(jogada_1 != null){
-            alert("Jogador 2 venceu")
+        resultFinal.textContent = resultFinal.textContent + "Jogador 2 venceu!";
+            ponto2 += 1;
         }
+        placar.style.display = "block";
     }
     else{
-        alert("Espere o contador chegar a 0")
+        ativarAlert();
     }
-    cont = 0;
-    setTimeout(document.getElementById('txt-contagem').textContent = "3", 1000);
-    setTimeout(controle = 0, 1000);
+    atualizarPontos();
     
 }
 // Adicionando jogadas pelo teclado
@@ -146,9 +187,7 @@ function teclado(e){
         tesoura_2()
     }
 }
-
 // Configurando contador
-
 function contador() {
   let time = 3;
   const Display = document.getElementById('txt-contagem');
@@ -164,4 +203,29 @@ function contador() {
     }
   }, 1000);
   
+}
+// Animação de esperar o contador
+const alert = document.getElementById('alert')
+
+function ativarAlert(){
+    alert.style.animation = "alertIn .5s";
+    
+    alert.style.display = "flex";
+    
+    setTimeout(() => {
+        fecharAlert();
+    }, 2000);
+}
+
+function fecharAlert(){
+    alert.style.animation = "alertOut .5s";
+
+    setTimeout(() => {
+        alert.style.display = "none";
+    },499)
+}
+// Pontuação
+function atualizarPontos(){
+    placar1.textContent = ponto1;
+    placar2.textContent = ponto2;
 }
